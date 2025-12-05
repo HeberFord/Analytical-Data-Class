31cad145f86accd446b99a0043ed0740a57f2e96
HeberFord <hhf5@duke.edu>
2025-12-05 09:32:42 -0500

    Updated with prop of sigfigs
    I updated the appropriate code to auto calculate sigfigs and then propogate them through addition, but not yet for other methods, I was running into machine epsilon issues with some of my code that I am still figuring out...

--- datum.py
+++ datum.py
@@ -4,22 +4,27 @@ import math
 
 def StatError(error, tol = 0.34):
     """
-    Docstring for StatError
-    
-    :param value: Description
-    :param error: Description
-    :param tol: Description
+    StateError: Function that determines the order of magnitude of the error \n
+    where to round when reporting data with signficant figures \n
+    determined by the first two non-zero digits in the error value \n
+    with an allowance of up to tol for intermediate insignificant figures. \n
+       
+    :Float value: The actual value of the measurment \n
+    :Float error: The actual error in the measurment\n
+    :Float tol: tolerance for number of insignificant digits to include by 1/N \n
+                for N insignificant digits max. Automatically set to 0.34 for \n
+                3 max insignificant digits before rounding
     """
     str_err = str(error)
     Vals = 0
     mod_err = ""
     err_ord = 0
     for char in str_err :
-        print(f"Current Char: {char}")
-        print(f"Current Vals: {Vals}")
+        #print(f"Current Char: {char}")
+        #print(f"Current Vals: {Vals}")
         if char == "0" and Vals == 0 or char == "." and Vals == 0:
             mod_err += char
-            print(mod_err)
+            #print(mod_err)
             continue
         else:
             while Vals < 2:
@@ -43,13 +48,14 @@ def StatError(error, tol = 0.34):
     err_ord += int(abs(math.log10(sig)// 1))
     #print(err_ord)
     return err_ord
-                        
-def Errorsig(error):
+
+def Errordet(error):
     """
-    Docstring for ErrorSig
+    Errorsig: The function determines the correct function to enact for \n
+    for different forms of error, whether statistical, propogated, or exact \n
     
-    :param value: Description
-    :param error: Description
+    :float value: The Actual Measurment value \n
+    :float error: The actual error in measurment \n
     """
     if error < 0:
         return ValueError("Error cannot be a negative value")
@@ -60,13 +66,40 @@ def Errorsig(error):
         return sig + 1
 
 def rd(value, order):
+    """
+    rd: A function designed in order to either round values to a specific \n
+    digit based on the order of the error input, or, if order is inf, \n
+    return the value \n
+    
+    :Float value: Value desired to be rounded \n
+    :Float order: Order of error in the value to be rounded \n
+    """
     if order == math.inf:
         return value
     else:
         rdv = round(value, abs(order))
         return rdv
     
-
+def sigfig(value): #Two issues rn, one if first sig fig is before decimals, and second is after, 
+    str_val = str(value)
+    sig = 0
+    dec = -1
+    for char in str_val :
+        if char == "0" and sig == 0 or char == "." and sig == 0:
+            if char == ".":
+                dec += 1
+            continue
+        elif char != ".":
+            if dec != -1:
+                dec += 1
+            sig += 1
+            continue
+        elif char == ".":
+            dec += 1
+            continue
+        else:
+            continue
+    return (sig, dec)
 
 class datum:
     """
@@ -74,28 +107,25 @@ class datum:
     """
     def __init__(self, value, Error, Units = "SI", Analyte = "Unk"):
         """
-        Docstring for __init___
-        
-        :param self: Description
-        :param value: Description
-        :param Error: Description
-        :param Units: Description
-        :param Analyte: Description
+        Docstring for __init___ \n
+        :param value: Description \n
+        :param Error: Description \n
+        :param Units: Description \n
+        :param Analyte: Description \n
         """
         self.value = value #Actual value of the variable of interest
         self.error = Error
         self.units = Units
         self.analyte = Analyte
-        self.errorder = Errorsig(self.error)
+        self.errorder = Errordet(self.error)
+        (self.sigfig, self.decsig) = sigfig(self.value)
     
     def __repr__(self):
         """
         Docstring for ___repr__
-        
-        :param self: Description
         """
         s = ""
-        s += f"{rd(self.value, self.errorder)} +/- {rd(self.error, self.errorder)} {self.units} ({self.analyte})"
+        s += f"{rd(self.value, self.errorder)} +/- {rd(self.error, self.errorder)} {self.units} ({self.analyte}) with ({self.sigfig}) significant figures"
         return s
 
     def __add__(self, other):
@@ -108,6 +138,9 @@ class datum:
         val = self.value + other.value
         err = math.sqrt(self.error**2 + other.error**2)
         result = datum(val, err, self.units, analyte) #Maybe preclude analyte to force people to define it later?
+        if result.decsig > self.decsig:
+            dif = result.decsig - self.decsig
+            (result.sigfig, result.decsig) = (result.sigfig - dif, self.decsig)
         return result
     
     def __sub__(self, other):
@@ -233,13 +266,16 @@ class datum:
         return self.value != other.value
 
 
+
+
 if __name__ == "__main__":
     d = datum(12.1167, 0.515, "m", "Length")
     print(d)
-    # d2 = datum(10.1234, 0.04333, "m", "Length")
-    # print(d2)
-    # d3 = d + d2
-    # print(d3)
+    print(sigfig(d.value))
+    d2 = datum(10.1234, 0.04333, "m", "Length")
+    print(d2)
+    d3 = d + d2
+    print(d3)
     # d4 = d * d2
     # print(d4)
     # d5 = d / d2
